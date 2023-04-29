694828ae8f2e00abc534166764e203e79424cf50
YangWooSong <olivia6844@naver.com>
2023-04-29 16:39:17 +0900

    StoryCredit

---
+++ Assets/Scripts/Credit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(new Vector2(transform.position.x, 0.3f*Time.deltaTime));
+        if(gameObject.transform.position.y > 7.5) Destroy(gameObject);
+    }
+}
