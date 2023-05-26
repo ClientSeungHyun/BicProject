9d36067549acfc8e7df04373ed8f52007375a4c3
mino <gksalsgh753@naver.com>
2023-05-26 22:49:45 +0900

    map setting, purchase asset folder make

---
+++ Assets/Scripts/Enemy/Missile.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    private Transform target; // 플레이어의 위치
+    private float speed = 10f; // 미사일 속도
+
+
+    private void Update()
+    {
+        SetTarget(GameObject.FindGameObjectWithTag("Player").transform);
+        if (target != null)
+        {
+            MoveTowardsTarget();
+        }
+    }
+
+    private void MoveTowardsTarget()
+    {
+        // 플레이어를 향해 미사일 이동
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+
+    public void SetTarget(Transform targetTransform)
+    {
+        // 플레이어 설정
+        target = targetTransform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 플레이어와 충돌 시 미사일 비활성화
+            gameObject.SetActive(false);
+        }
+    }
+
+
+  
+
+    
+
+}
